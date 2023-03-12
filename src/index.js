a37f90bb0c02189ff8043a4fb7e36e8b0f184dfa
Achmad F. Ibrahim <acfatah@gmail.com>
2023-03-12 13:41:34 +0800

    Add capitalize and pascalCase module to main entry file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { camelCase, camelCaseKeys, recursiveCamelCaseKeys } from './camel-case'
+export { capitalize } from './capitalize'
+export { pascalCase } from './pascal-case'
 export { snakeCase, snakeCaseKeys, recursiveSnakeCaseKeys } from './snake-case'
