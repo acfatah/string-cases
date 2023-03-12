6751a02749c7f98791629451ec63233b2776b397
Achmad F. Ibrahim <acfatah@gmail.com>
2023-03-12 13:40:46 +0800

    Remove unused commented code

--- src/pascal-case.js
+++ src/pascal-case.js
@@ -8,26 +8,3 @@ export const pascalCase = string => {
     .replace(/([-_.\s][a-z0-9])/g, char => char[1].toUpperCase())
     .replace(/\b[a-z]/g, char => char.toUpperCase())
 }
-
-// export const pascalCaseKeys = object => Object.keys(object).reduce(
-//   (result, key) => Object.assign(result, { [pascalCase(key)]: object[key] }), {}
-// )
-
-// export const recursivePascalCaseKeys = object => {
-//   if (
-//     object == null
-//     || typeof object !== 'object'
-//     || object instanceof Date
-//     || object instanceof RegExp
-//   ) return object
-
-//   if (Array.isArray(object)) {
-//     return object.map(value => recursivePascalCaseKeys(value))
-//   }
-
-//   return Object.keys(object).reduce((acc, key) => {
-//     acc[pascalCase(key)] = recursivePascalCaseKeys(object[key])
-
-//     return acc
-//   }, {})
-// }
