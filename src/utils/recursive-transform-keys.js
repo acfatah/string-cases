0a169d78187ad5f43f7e5194dbe3afe4bad7fbae
Achmad F. Ibrahim <acfatah@gmail.com>
2023-12-31 23:38:09 +0800

    Refactor to reduce Cognitive Complexity

--- src/utils/recursive-transform-keys.js
+++ src/utils/recursive-transform-keys.js
@@ -1,10 +1,14 @@
 import { objectWithInvalidKeys } from './object-with-invalid-keys'
 
+const transformArrayKeys = (transformFunc, array) => {
+  return array.map(value => recursiveTransformKeys(transformFunc)(value))
+}
+
 export const recursiveTransformKeys = (transformFunc) => (object) => {
   if (objectWithInvalidKeys(object)) return object
 
   if (Array.isArray(object)) {
-    return object.map(value => recursiveTransformKeys(transformFunc)(value))
+    return transformArrayKeys(transformFunc, object)
   }
 
   return Object.keys(object).reduce((acc, key) => {
